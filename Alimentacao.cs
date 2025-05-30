ed4aa157e51cb99ac0b62ad47db843b465d061d0
Rafaela Ferreira <142462851+cc23015@users.noreply.github.com>
2025-05-29 21:12:59 -0300

    Primeiro commit

---
+++ Alimentacao.cs
@@ -0,0 +1,11 @@
+﻿namespace DespesasGerais
+{
+    public class Alimentacao
+    {
+        public int Id { get; set; }
+        public string Alimento { get; set; }
+        public string Local { get; set; }
+        public double Valor { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
---
+++ DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DespesasGerais
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Alimentacao> Alimentos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=localhost; Database=DB23015; UserId=DB23015; Password:DB23015; MultipleActiveResultSets=true; Encrypt=TES; TrustServerCertificate=YES");
+    }
+}
+
